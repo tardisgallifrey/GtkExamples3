4b24e23bee18b8e4c13a7897b3b0a2ff4f7f8cc5
Dave <bigmanrunning@gmail.com>
2020-10-27 18:22:52 -0500

    GtkExceptionTest 10/27/2020

---
+++ GExceptionTest/Program.cs
@@ -0,0 +1,38 @@
+﻿// HelloWorld.cs - GTK Window class Test implementation
+//
+// Author: Mike Kestner <mkestner@speakeasy.net>
+//
+// (c) 2001-2002 Mike Kestner
+
+//modified 10/27/2020
+//moved from class definition to Program.cs definition
+using Gtk;
+
+
+namespace GExceptionTest
+{
+
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+            Application.Init();
+            Window win = new Window("GException");
+
+            //This is a better way to handle the exception than the original
+            try
+            {
+                win.SetIconFromFile("this.filename.does.not.exist");
+                // Notreached, GException should throw on above call.
+
+            }
+            finally
+            {
+                Application.Quit();
+            }
+
+
+            return 0;
+        }
+    }
+}
